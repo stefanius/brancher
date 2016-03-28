98950c04514901ca090e4232f87961ebe7745360
Stefanius <sgrootveld@gmail.com>
2016-03-28 21:18:54 +0200

    Removed unused import.

--- src/Stefanius/Brancher/Command/CheckCommand.php
+++ src/Stefanius/Brancher/Command/CheckCommand.php
@@ -3,7 +3,6 @@
 namespace Stefanius\Brancher\Command;
 
 use Stefanius\Brancher\Checker\Checker;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
