a3b05ed5ce917f767d9955db0413c51cf585a193
Stefanius <sgrootveld@gmail.com>
2016-04-13 19:23:39 +0200

    Implement setup for GitHub and Jira support.

--- src/Stefanius/Brancher/Command/SetupCommand.php
+++ src/Stefanius/Brancher/Command/SetupCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Yaml\Dumper;
 
 class SetupCommand extends BaseCommand
 {
@@ -36,22 +37,90 @@ class SetupCommand extends BaseCommand
         $answer = $helper->ask($input, $output, $systemQuestion);
         $configMethod = sprintf('setup%sConfig', ucfirst($answer));
 
-        $this->$configMethod();
+        $this->$configMethod($input, $output);
+    }
+
+    /**
+     * @param array $config
+     */
+    protected function writeYamlData(array $config)
+    {
+        $dumper = new Dumper();
+
+        $yamlData = $dumper->dump($config, 4);
+
+        file_put_contents('test', $yamlData);
     }
 
     /**
      * Setup the config for a Jira system.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
      */
-    private function setupJiraConfig()
+    private function setupJiraConfig(InputInterface $input, OutputInterface $output)
     {
-        var_dump('piet');
+        $helper = $this->getHelper('question');
+
+        $answers = [];
+
+        $questions = [
+            'username' => new Question('Please enter your username: '),
+            'password' => new Question('Please enter your password: '),
+            'host'     => new Question('Please enter the Jira host: '),
+        ];
+
+        foreach ($questions as $key => $question) {
+            $answers[$key] = $helper->ask($input, $output, $question);
+        }
+
+        $answers['auth'] = base64_encode(sprintf('%s:%s', $answers['username'], $answers['password']));
+
+        unset($answers['username']);
+        unset($answers['password']);
+
+        $config = [
+            'settings' => [
+                'primary_connection'   => 'jira',
+                'secondary_connection' => 'jira',
+            ],
+            'github' => $answers,
+        ];
+
+        $this->writeYamlData($config);
     }
 
     /**
      * Setup the config for a Github system.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
      */
-    private function setupGithubConfig()
+    private function setupGithubConfig(InputInterface $input, OutputInterface $output)
     {
-        var_dump('henk');
+        $helper = $this->getHelper('question');
+
+        $answers = [];
+
+        $questions = [
+            'username' => new Question('Please enter your username: '),
+            'owner'    => new Question('Please enter the repository owner: '),
+            'repo'     => new Question('Please enter your repository: '),
+            'token'    => new Question('Please enter the token: '),
+        ];
+
+        foreach ($questions as $key => $question) {
+            $answers[$key] = $helper->ask($input, $output, $question);
+        }
+
+        $config = [
+            'settings' => [
+                'primary_connection'   => 'github',
+                'secondary_connection' => 'github',
+            ],
+            'github' => $answers,
+        ];
+
+        $this->writeYamlData($config);
     }
 }
